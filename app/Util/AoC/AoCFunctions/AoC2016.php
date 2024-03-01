a42820df4c9c3c67f681c695e1e2b6a00c3aae23
jkcieslak <k.k.cieslak@wp.pl>
2024-03-01 16:27:54 +0100

    AoC2016 Day 2

--- app/Util/AoC/AoCFunctions/AoC2016.php
+++ app/Util/AoC/AoCFunctions/AoC2016.php
@@ -103,4 +103,71 @@ class AoC2016
 
         throw new AoCException("No solution found.");
     }
+
+    /**
+     * @throws AoCException
+     */
+    public static function puzzle_2016_2_1(string $input) : int {
+        $code = 0;
+        $button = 5;
+        $digitInstructions = explode(PHP_EOL, trim($input));
+        foreach ($digitInstructions as $digitInstruction) {
+            $buttonInstructions = str_split($digitInstruction, 1);
+            foreach ($buttonInstructions as $buttonInstruction) {
+                $button = match ($buttonInstruction) {
+                    'U' => ($button >= 4) ? $button - 3 : $button,
+                    'D' => ($button <= 6) ? $button + 3 : $button,
+                    'L' => ($button % 3 != 1) ? $button - 1 : $button,
+                    'R' => ($button % 3 != 0) ? $button + 1 : $button,
+                    default => throw new AoCException("Invalid code instruction"),
+                };
+            }
+            $code = $code * 10 + $button;
+        }
+        return $code;
+    }
+
+    /**
+     * @throws AoCException
+     */
+    public static function puzzle_2016_2_2(string $input) : string {
+        $code = [];
+        $codePad = [
+            [0, 0, 1, 0, 0],
+            [0, 2, 3, 4, 0],
+            [5, 6, 7, 8, 9],
+            [0, 'A', 'B', 'C', 0],
+            [0, 0, 'D', 0, 0]
+        ];
+        $y = 2;
+        $x = 0;
+        $digitInstructions = explode(PHP_EOL, trim($input));
+        foreach ($digitInstructions as $digitInstruction) {
+            $buttonInstructions = str_split($digitInstruction, 1);
+            foreach ($buttonInstructions as $buttonInstruction) {
+                switch ($buttonInstruction) {
+                    case 'U':
+                        if(isset($codePad[$y-1][$x]) && $codePad[$y-1][$x] != 0)
+                            $y--;
+                        break;
+                    case 'D':
+                        if(isset($codePad[$y+1][$x]) && $codePad[$y+1][$x] != 0)
+                            $y++;
+                        break;
+                    case 'L':
+                        if(isset($codePad[$y][$x-1]) && $codePad[$y][$x-1] != 0)
+                            $x--;
+                        break;
+                    case 'R':
+                        if(isset($codePad[$y][$x+1]) && $codePad[$y][$x+1] != 0)
+                            $x++;
+                        break;
+                    default:
+                        throw new AoCException("Invalid code instruction");
+                }
+            }
+            $code[] = $codePad[$y][$x];
+        }
+        return implode('', $code);
+    }
 }
