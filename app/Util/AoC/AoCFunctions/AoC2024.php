a9714088ab3aa0d9ca7170ddc86ebbb3b45bd6fa
Zoom <k.k.cieslak@wp.pl>
2024-12-08 00:44:23 +0100

    Added AoC 2024 days 2 and 3 solution

--- app/Util/AoC/AoCFunctions/AoC2024.php
+++ app/Util/AoC/AoCFunctions/AoC2024.php
@@ -61,4 +61,144 @@ class AoC2024
 
         return $similarity;
     }
+
+    public static function puzzle_2024_2_1(string $input): int {
+        $data = explode(PHP_EOL, trim($input));
+        $data = array_map(fn (string $report) => explode(' ', $report), $data);
+
+        $totalSafe = 0;
+
+        foreach ($data as $report) {
+            $growth = $report[1] > $report[0];
+            $safe = true;
+            for ($i = 1; $i < count($report); $i++) {
+                //is growth the same as starting out
+                if (($report[$i] > $report[$i-1]) !== $growth) {
+                    $safe = false;
+                    break;
+                }
+                $diff = abs($report[$i] - $report[$i-1]);
+                if ($diff < 1 || $diff > 3) {
+                    $safe = false;
+                    break;
+                }
+            }
+            $totalSafe += $safe ? 1 : 0;
+        }
+
+        return $totalSafe;
+    }
+    public static function puzzle_2024_2_2(string $input): int {
+        $data = explode(PHP_EOL, trim($input));
+        $data = array_map(fn (string $report) => explode(' ', $report), $data);
+
+        $totalSafe = 0;
+
+        foreach ($data as $report) {
+            if (self::isReportSafe($report)) {
+                $totalSafe++;
+                continue;
+            }
+
+            for ($i = 0; $i < count($report); $i++) {
+                $temp = $report;
+                array_splice($report, $i, 1);
+
+                if (self::isReportSafe($report)) {
+                    $totalSafe++;
+                    continue 2;
+                }
+
+                $report = $temp;
+            }
+        }
+
+        return $totalSafe;
+    }
+
+    //report is safe when both of the conditions are true :
+    ////it's values increase or deacrease throughout e.g. 1 3 5 or 8 6 3
+    //difference between elements must be in {1, 2, 3} set
+    public static function isReportSafe(array $report): bool {
+        if (!self::isArraySorted($report)) {
+            return false;
+        }
+        if (!self::areArrayIncrementsSafe($report)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    //this considers 2, 3, 3, 4 unsorted
+    public static function isArraySorted(array $array): bool {
+        $signSum = 0;
+
+        for ($i = 0; $i < count($array) - 1; $i++) {
+            $signSum += self::sign($array[$i+1] - $array[$i]);
+        }
+
+        return abs($signSum) === count($array) - 1;
+    }
+
+    public static function sign(int $number): int {
+        return ($number > 0) - ($number < 0);
+    }
+
+    public static function areArrayIncrementsSafe(array $array): bool {
+        $safe = 0;
+
+        for($i = 0; $i < count($array) - 1; $i++) {
+            $diff = abs($array[$i+1] - $array[$i]);
+            if ($diff > 3 || $diff < 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static function puzzle_2024_3_1(string $input): int {
+        $mults = [];
+        $multsNo = preg_match_all('/mul\([0-9]+\,[0-9]+\)/', $input, $mults);
+
+        $finalSum = 0;
+
+        foreach ($mults[0] as $mult) {
+            $nums = [];
+            preg_match_all('/[0-9]+/', $mult, $nums);
+            $finalSum += $nums[0][0] * $nums[0][1];
+        }
+
+        return $finalSum;
+    }
+
+    public static function puzzle_2024_3_2(string $input): int
+    {
+        $commands = [];
+        $commandsNo = preg_match_all('/mul\([0-9]+\,[0-9]+\)|do\(\)|don\'t\(\)/', $input, $commands);
+
+        $finalSum = 0;
+        $multEnabled = true;
+
+        foreach ($commands[0] as $command) {
+            switch ($command) {
+                case 'do()':
+                    $multEnabled = true;
+                    break;
+                case 'don\'t()':
+                    $multEnabled = false;
+                    break;
+                default:
+                    if ($multEnabled) {
+                        $mult = [];
+                        preg_match_all('/[0-9]+/', $command, $mult);
+                        $finalSum += $mult[0][0] * $mult[0][1];
+                    }
+                    break;
+            }
+        }
+
+        return $finalSum;
+    }
 }
+
